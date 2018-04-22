41c77f1b17225249bc8d3332da2cdcc5a09d1c44
Molly Zurheide <you@example.com>
2018-04-21 22:18:48 -0400

    a little progress

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,72 +1,67 @@
-var apiKey = "rgefqvI6qcj2vCUzG1vz8YPeb2wKIKEZ";
-
 var newBreed = "";
 
 var breedsArray = ["poodle", "golden retriever", "springer spaniel", "dachsund", "chihuahua", "brittany spaniel", "collie", "greyhound", "chocolate lab", "shih tzu", "pug", "visla"];
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newBreed + "&limit=10" + "&rating=pg" + "&api_key=" + apiKey;
-
-//NEED TO DEFINE RESPONSE??
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-    
-  }).then(function(response) {
-    
-    console.log(response);
-    //HOW DOES BROWSER KNOW WHAT RESPONSE IS??
-    
-    
-//statically load form to submit dog breed types, AND pre-loaded dog breed buttons??
+//statically load form to submit dog breed types, AND pre-loaded dog breed buttons????
 
 //function to create buttons for items in breeds array
 function addButtons() {
     $("#button-container").empty(); //delete existing buttons to avoid repeats
-    for (var i=0; i<breedsArray.length; i++) {
+    for (var i = 0; i < breedsArray.length; i++) {
+        //console.log("hello?") - WORKS!!;
         var dogButton = $("<button>");
-        dogButton.addClass(".dog-button");
+        dogButton.addClass("dog-button");
         dogButton.attr("data-breed", breedsArray[i]);
         dogButton.text(breedsArray[i]);
         $("#button-container").append(dogButton);
     }
 }
 
-addButtons();
-
-
 //NEED TO DISPLAY INITIAL BUTTONS *AND* ONES THAT ARE ADDED!!!!!
 //when new dog breed is submitted, push to array and create button
-$("#submit-button").on("click", function(event) {
-    event.preventDefault();
-    var newBreed = $("#breed-field").val().trim();
-    breedsArray.push(newBreed); //SHOULD ONLY ADD ONCE ON CLICK
-    addButtons();
-    console.log("hello")
-}); //how to do in real time?
+$(document).ready(function() { //on load...
+    addButtons(); //add buttons for breeds in pre-loaded array
 
-
-//when a dog breed button is clicked, load 10 gifs related to that theme plus each gif's rating
-$(".dog-button").on("click", function() {
-//    var rating = results.rating;
-//    var stillImage;
-//    var movingImage;
-    $("#gif-container").empty(); //clear gifs from last button click
-    for (var i=0; i<10; i++) {
-        var results = response.data[i];
-        var stillImageUrl = results.images.fixed_height_still.url;
-        var movingImageUrl = results.images.fixed_height.url;
-        var stillImage = $("<img>");
-        stillImage.attr("src", stillImageUrl[i]);
-        $("#gif-container").prepend(stillImage);
-        stillImage.prepend(rating);
-        stillImage.on("click", function() { //when still image clicked, it starts moving
-            stillImage.attr("src", movingImageUrl[i]);
+    $(".dog-button").on("click", function() { //when dog breed button clicked, load 10 gifs related to that breed
+        //console.log("hey!") - WORKS!!;
+        var newBreed = $("#breed-field").val().trim(); //capture new breeds entered by user
+        var apiKey = "";
+        var url = "https://api.giphy.com/v1/gifs/search" //EXISTING BUTTONS SHOULD ALSO LOAD GIFS!!!!!!!!!!!!!!!!!!!!
+        url += "?" + "q=" + $.param ({
+            'q': newBreed, //NOT CAPTURING SEARCH TERMS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            'apiKey': "rgefqvI6qcj2vCUzG1vz8YPeb2wKIKEZ"
         });
-        var movingImage = $("<img>"); //when moving image clicked, it stops moving
-        movingImage.on("click", function() {
-            movingImage.attr("src", stillImageUrl[i])
+        console.log(url);
+        //var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newBreed + "&limit=10" + "&rating=pg" + "&api_key=" + apiKey;
+        //var queryURL = "https://api.giphy.com/v1/gifs/search?q=dogs+&limit=10+&rating=pg+&api_key=rgefqvI6qcj2vCUzG1vz8YPeb2wKIKEZ";
+        $.ajax({
+            url: url,
+            method: "GET"
+        }).then(function(response) {
+            $("#submit-button").on("click", function(event) { //when submit button is clicked...
+                var newBreed = $("#breed-field").val().trim(); //capture new breeds entered by user
+                event.preventDefault();
+                breedsArray.push(newBreed); //SHOULD ONLY ADD ONCE ON CLICK //push new breeds to breed array
+                //console.log("hello") WORKS;
+                addButtons(); //add buttons for new array items
+            });
+            for (var i = 0; i < response.data.length; i++) {
+                //console.log("what?") - works with updated queryURL;
+                var rating = response.data[i].rating;
+                var stillImageUrl = response.data[i].images.fixed_height_still.url;
+                var movingImageUrl = response.data[i].images.fixed_height.url;
+                var stillImage = $("<img>");
+                stillImage.attr("src", stillImageUrl);
+                $("#gif-container").prepend(stillImage);
+                stillImage.prepend(rating);
+                stillImage.on("click", function() { //when still image clicked, it starts moving
+                    stillImage.attr("src", movingImageUrl);
+                    var movingImage = $("<img>"); //when moving image clicked, it stops moving
+                        movingImage.on("click", function() {
+                            movingImage.attr("src", stillImageUrl)
+                        });
+                });
+            }
         });
-    }
 });
 });
