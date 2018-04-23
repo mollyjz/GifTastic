db1ba1b5fb67dba7d7f9f09ce4077e3aa3a8213b
Molly Zurheide <you@example.com>
2018-04-23 19:38:12 -0400

    more updates

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,22 +3,20 @@ var newBreed = "";
 var breedsArray = ["poodle", "golden retriever", "springer spaniel", "dachsund", "chihuahua", "brittany spaniel", "collie", "greyhound", "chocolate lab", "shih tzu", "pug", "visla"];
 var dogButton;
 
-//NOW, PREEXISTING BUTTONS DON'T LOAD GIFS UNTIL YOU'VE CLICKED AN ADDED BUTTON!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-function addButtons() { //function to add buttons for items in array                                    //WORKS
-    $("#button-container").empty(); //delete existing buttons to avoid repeats
-    for (var i = 0; i < breedsArray.length; i++) { //create button for each item in dog breeds array
-        var dogButton = $("<button>"); //create and store button
-        dogButton.addClass("dog-button"); //add class
-        dogButton.attr("data-breed", breedsArray[i]);  //ONLY CAPTURING FIRST BREED IN ARRAY!!!!!!!!!!!!!!!!!!!
-        dogButton.text(breedsArray[i]); //add text of each breed to its button
-        $("#button-container").append(dogButton); //append button to page
-        //console.log(breedsArray[i])
-        //console.log(breed) doesn't work                                                               //WORKS
-    }
-}
-
 $(document).ready(function() { //on page load...
+    function addButtons() { //function to add buttons for items in array                                    //WORKS
+        $("#button-container").empty(); //delete existing buttons to avoid repeats
+        for (var i = 0; i < breedsArray.length; i++) { //create button for each item in dog breeds array
+            var dogButton = $("<button>"); //create and store button
+            dogButton.addClass("dog-button"); //add class
+            dogButton.attr("data-breed", breedsArray[i]);  //ONLY CAPTURING FIRST BREED IN ARRAY!!!!!!!!!!!!!!!!!!!
+            dogButton.text(breedsArray[i]); //add text of each breed to its button
+            $("#button-container").append(dogButton); //append button to page
+            //console.log(breedsArray[i])
+            console.log(dogButton.breed) //*******************************************************************
+            }
+    }
+ 
     addButtons(); //add buttons for breeds in pre-loaded array                                          //WORKS
     $("#submit-button").on("click", function(event) { //any time submit button is clicked...
         event.preventDefault();
@@ -27,8 +25,9 @@ $(document).ready(function() { //on page load...
         //console.log("this is working!");
         addButtons(); //add button for each new array item
         //console.log(data-breed);
-    $(".dog-button").on("click", function() { //when dog breed button clicked...                    //NOT WORKING!!!!!
-        var buttonText = $(".dog-button").data("breed"); //capturing text of that dog button
+    });
+    $("#button-container").on("click", ".dog-button", function() { //when dog breed button clicked...                    //NOT WORKING!!!!!
+        var buttonText = $(this).data("breed"); //capturing text of each dog button
         var url = "https://api.giphy.com/v1/gifs/search";
         url += "?" + $.param({
             'q': buttonText, //NOW, NO MATTER WHAT BUTTON IS CLICKED, 'POODLE' IS ALWAYS THE SEARCH TERM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -40,25 +39,31 @@ $(document).ready(function() { //on page load...
             url: url,
             method: "GET"
         }).then(function(response) {                                                                //NOT WORKING!!!!!
-            console.log(url);
+            //console.log(url);
+            $("#gif-container").on("click", ".still-image", function() { //when still image clicked...
+                var imageGif = $(this); //refers to the image you've clicked
+                if (imageGif.attr("src") === imageGif.attr("data-moving")) { //if url of image = moving image url
+                    imageGif.attr("src", "data-still"); //assign still image url
+                    $("<img>");  //CHANGES SRC BUT DELETES IMG ON CLICK!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                } else { //otherwise...
+                    imageGif.attr("src", "data-moving"); //assign data moving url to make it move
+                    $("<img>");
+                }
+            });
             for (var i = 0; i < response.data.length; i++) { //for each of the 10 gif responses from the api...
                 //console.log("this works!");
                 var rating = response.data[i].rating; //grab that gif's rating from api data
                 var stillImageUrl = response.data[i].images.fixed_height_still.url; //store URL for still image
                 var movingImageUrl = response.data[i].images.fixed_height.url; //store URL for animated gif
                 var stillImage = $("<img>"); //create variable to store still image
+                stillImage.attr("data-moving", movingImageUrl);
+                //movingImage.attr("data-still", stillImageUrl);
                 stillImage.attr("src", stillImageUrl); //assign URL to still image
+                stillImage.addClass("still-image");
                 $("#gif-container").prepend(stillImage); //prepend still image to container
                 stillImage.prepend(rating); //prepend rating to still image
-                stillImage.on("click", function() { //when still image clicked...
-                    stillImage.attr("src", movingImageUrl); //change URL to that of the moving gif so it starts moving
-                    var movingImage = $("<img>"); //create variable to store moving image
-                        movingImage.on("click", function() { //when moving image clicked, ...
-                            movingImage.attr("src", stillImageUrl) //change src back to that of the still image so that it stops moving
-                        }); // ^ ONLY WORKING FOR FIRST IMAGE IN ARRAY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                });
+                
             }
         });
-});
 });
 });
